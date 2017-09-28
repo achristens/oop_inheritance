35b63a28e7aefd5056fa00792fcb80d72f9a12fa
Abby Christens <achristens@gmail.com>
2017-09-28 14:55:33 -0400

    Adding quote collector child

---
+++ quote_collector.rb
@@ -0,0 +1,34 @@
+require './multilinguist.rb'
+
+class QuoteCollector < Multilinguist
+  # Each instance of this class should have the ability to select a random quote to share in the local language.
+  @@quotes = []
+
+  # Instance Method
+  def add_quote(new_quote)
+    @@quotes << new_quote
+  end
+
+  # Class Method
+  def self.all_quotes
+    @@quotes
+  end
+
+  def share_quote
+    puts say_in_local_language("Here's a quote for you, #{@@quotes.sample}.")
+  end
+
+end
+
+me = QuoteCollector.new
+
+puts me.inspect
+me.add_quote("The grass is always greener")
+me.add_quote("Raining like cats and dogs")
+me.add_quote("The pot calling the kettle black")
+#
+# QuoteCollector.share_quote
+me.travel_to("India")
+me.share_quote
+me.travel_to("Italy")
+me.share_quote
